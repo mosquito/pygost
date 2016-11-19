536ca5f1627945c28620f9dacb5900fc8864cb8c
Sergey Matveev <stargrave@stargrave.org>
2016-11-19 22:21:51 +0300

    Missing copyright and module docstring in gost3410_vko

--- pygost/gost3410_vko.py
+++ pygost/gost3410_vko.py
@@ -1,3 +1,22 @@
+# coding: utf-8
+# PyGOST -- Pure Python GOST cryptographic functions library
+# Copyright (C) 2015-2016 Sergey Matveev <stargrave@stargrave.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""Diffie-Hellman functions, VKO GOST R 34.10-2001/2012
+"""
+
 from pygost.gost3410 import pub_marshal
 from pygost.gost34112012256 import GOST34112012256
 from pygost.gost34112012512 import GOST34112012512
