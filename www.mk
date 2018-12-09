07d29d4f702fa5db2c3992f4312437d3ed8ec09b
Sergey Matveev <stargrave@stargrave.org>
2018-12-09 13:12:21 +0300

    Website restyling

--- www.mk
+++ www.mk
@@ -2,10 +2,12 @@ all: pygost.html
 
 MAKEINFO ?= makeinfo
 
+CSS != cat style.css
+
 pygost.html: *.texi
 	rm -f pygost.html/*.html
 	$(MAKEINFO) --html \
-		--set-customization-variable NO_CSS=1 \
+		--set-customization-variable CSS_LINES='$(CSS)' \
 		--set-customization-variable SHOW_TITLE=0 \
 		--set-customization-variable USE_ACCESSKEY=0 \
 		--set-customization-variable DATE_IN_HEADER=1 \
