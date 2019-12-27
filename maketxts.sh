c80c7551777aaf4deb880b108870e6f889034a53
Sergey Matveev <stargrave@stargrave.org>
2019-12-27 13:43:32 +0300

    Autogenerate INSTALL/NEWS

---
+++ maketxts.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -ex
+
+texi=$(mktemp)
+
+cat > $texi <<EOF
+\input texinfo
+@documentencoding UTF-8
+@settitle NEWS
+@node News
+@unnumbered News
+`sed -n '3,$p' < news.texi`
+@bye
+EOF
+makeinfo --plaintext -o NEWS $texi
+
+cat > download.texi <<EOF
+You can obtain releases source code prepared tarballs on
+@url{http://pygost.cypherpunks.ru/}.
+EOF
+
+perl -i -p -e 's/hash=sha256:\w+/hash=sha256:TARBALL-HASH/' install.texi
+
+cat > $texi <<EOF
+\input texinfo
+@documentencoding UTF-8
+@settitle INSTALL
+@include install.texi
+@bye
+EOF
+makeinfo --plaintext -o INSTALL $texi
+
+rm -f $texi
+
+git checkout download.texi install.texi
